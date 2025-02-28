5ed7cf9b0bb1e82758c7d90dd9a8723875f232ef
ClyheR <121443121+ClyheR@users.noreply.github.com>
2025-02-28 21:53:42 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,55 @@
 const express = require('express');
 const fs = require('fs');
+const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { prototype } = require('events');
+let cors=require("cors")
 require('dotenv').config()
 const app = express();
 app.use(express.urlencoded({ extended : true }));
 app.use(express.json());
 app.set('view engine', 'ejs');
 app.use(express.static('public')); // Serve static files (CSS, images)
+app.use(cors())
 app.use(cookieParser()); // Enable cookies
 app.get('/', (req, res) => {
     const user = req.cookies.user || null; // Check for user login cookie
     res.render('index', { user });
 });
+
+async function connected(){
+  try{
+      await mongoose.connect('mongodb+srv://nishantadvani724:kPpWtMq8ZR5K4kMN@cluster0.qh81f.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }).then(()=>{
+        console.log("databse connected")
+      }).catch((err)=>{
+        if(err){
+          console.log("databse not connected")    
+        }
+        
+      })
+  }catch(err){
+    if(err){
+      console.log("some error comes in the database")
+    }
+  }
+     
+}
+
+connected();
+
+
+
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String
+});
+
+ const User = mongoose.model('User', userSchema);
 
 // PC Build Details Page
 app.get('/build/:id', (req, res) => {
@@ -34,8 +71,33 @@ app.get('/login', (req, res) => {
   res.render('login');
 });
 
+//simulated logins
+app.post('/logins',async(req,res)=>{
+try{
+  const {email , password}= req.body;
+  let data=await User.find({email,password});
+
+
+
+  if(data){
+  
+    res.cookie('user', JSON.stringify({name:data[0].name, email: email, password: password }), { maxAge: 3600000 });
+    res.redirect("/");
+  }
+  const user = req.cookies?.user // Check for user login cookie
+  res.render('index', { user });
+
+}catch(err){
+  if(err){
+    console.log("not found")
+  }
+}
+
+})
+
 // Sign-up Page
 app.get('/signin', (req, res) => {
+  
   if (req.cookies.user) {
       return res.redirect('/');
   }
@@ -43,11 +105,33 @@ app.get('/signin', (req, res) => {
 });
 
 // Simulated Login (For Now)
-app.post('/signin', (req, res) => {
-  const { name,email, password } = req.body;
-  res.cookie('user', JSON.stringify({ name: name, email:email,password:password}), { maxAge: 3600000 });
-  res.redirect('/');
+// app.post('/signin', (req, res) => {
+//   const { name,email, password } = req.body;
+
+// this the testing code for signin
+app.post('/signin', async (req, res) => {
+  try{
+    const { name, email, password } = req.body;
+
+    const user = new User({ name, email, password });
+
+
+    await user.save();
+    res.cookie('user', JSON.stringify({ name: name, email: email, password: password }), { maxAge: 3600000 });
+    res.redirect('/');
+  }catch(err){
+    console.log("data is not send")
+  }
+  
+  
 });
+//from here to the testing signin delete
+
+
+
+//   res.cookie('user', JSON.stringify({ name: name, email:email,password:password}), { maxAge: 3600000 });
+//   res.redirect('/');
+// });
 
 // Simulated Logout
 app.get('/logout', (req, res) => {
